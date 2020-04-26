98b73a1f80b98d9b9d149f530774516797c0c653
edwin <edwin@LAPTOP-0FJJA924>
2020-04-26 18:41:22 -0500

    Proceso de llenado de los Jpanel

--- SIPV/src/Mesas.java
+++ SIPV/src/Mesas.java
@@ -6,32 +6,70 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.JRadioButton;
+import javax.swing.JTable;
+import java.awt.Checkbox;
+import javax.swing.JInternalFrame;
+import javax.swing.JCheckBox;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class Mesas extends JPanel {
 	
-
-	/**
-	 * Create the panel.
-	 */
+	
+	
+	
 	public Mesas() {
-		JPanel panelMesas = new JPanel();
-		panelMesas.setBackground(Color.PINK);
+	
+	
+
+	    JPanel panelMesas = new JPanel();
+		panelMesas.setBackground(Color.WHITE);
 		panelMesas.setBounds(147, 6, 597, 410);
-		panelMesas.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Aceptar");
+		btnNewButton.setBounds(353, 281, 89, 23);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(411, 242, 89, 23);
+		panelMesas.setLayout(null);
 		panelMesas.add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("Logistica de mesas");
+		JLabel lblNewLabel = new JLabel("Log\u00EDstica De Mesas");
+		lblNewLabel.setBounds(222, 11, 238, 23);
 		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
-		lblNewLabel.setBounds(167, 22, 238, 14);
 		panelMesas.add(lblNewLabel);
 		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Mesa 1", "Mesa 2", "Mesa 3", "Mesa 4", "Mesa 5"}));
+		comboBox.setBounds(359, 104, 126, 25);
+		panelMesas.add(comboBox);
+		
+		JLabel lblOpMesas = new JLabel("Seleccione la mesa ");
+		lblOpMesas.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		lblOpMesas.setBounds(73, 102, 191, 27);
+		panelMesas.add(lblOpMesas);
+		
+		JButton btnNewButton_1 = new JButton("Salir");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnNewButton_1.setBounds(452, 281, 89, 23);
+		panelMesas.add(btnNewButton_1);
+		
+		JLabel lblOporden = new JLabel("\u00BFLevantar orden?");
+		lblOporden.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		lblOporden.setBounds(73, 168, 191, 27);
+		panelMesas.add(lblOporden);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("S\u00ED");
+		rdbtnNewRadioButton.setBounds(359, 172, 50, 23);
+		panelMesas.add(rdbtnNewRadioButton);
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("No");
+		rdbtnNewRadioButton_1.setBounds(432, 172, 50, 23);
+		panelMesas.add(rdbtnNewRadioButton_1);
+		
+		
 	}
-
 }
--- SIPV/src/Pedidos.java
+++ SIPV/src/Pedidos.java
@@ -5,6 +5,8 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JSpinner;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class Pedidos extends JPanel {
 
@@ -19,33 +21,49 @@ public class Pedidos extends JPanel {
 		Pedidos.setLayout(null);
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Lista de pedidos");
-		lblNewLabel.setBounds(78, 11, 229, 29);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		JLabel lblNewLabel = new JLabel("Lista De Pedidos");
+		lblNewLabel.setBounds(26, 11, 229, 29);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 18));
 		add(lblNewLabel);
 		
 		JButton btnNewButton = new JButton("Enviar");
-		btnNewButton.setBounds(80, 265, 82, 29);
+		btnNewButton.setBounds(26, 247, 98, 29);
 		add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Cancelar");
-		btnNewButton_1.setBounds(250, 265, 98, 29);
+		btnNewButton_1.setBounds(157, 247, 98, 29);
 		add(btnNewButton_1);
 		
-		JList list = new JList();
-		list.setBounds(78, 139, 186, 29);
-		add(list);
-		
 		JLabel lblNewLabel_1 = new JLabel("Platillos");
-		lblNewLabel_1.setBounds(74, 111, 61, 16);
+		lblNewLabel_1.setBounds(26, 82, 61, 21);
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 17));
 		add(lblNewLabel_1);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setBounds(289, 142, 61, 26);
+		spinner.setBounds(228, 114, 67, 29);
 		add(spinner);
 		
 		JLabel lblNewLabel_2 = new JLabel("imagen");
-		lblNewLabel_2.setBounds(349, 69, 95, 143);
+		lblNewLabel_2.setBounds(349, 21, 95, 255);
 		add(lblNewLabel_2);
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setBounds(228, 181, 67, 29);
+		add(spinner_1);
+		
+		JLabel lblBebidas = new JLabel("Bebidas");
+		lblBebidas.setBounds(26, 157, 61, 21);
+		lblBebidas.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+		add(lblBebidas);
+		
+		JComboBox platillos = new JComboBox();
+		platillos.setModel(new DefaultComboBoxModel(new String[] {"Hamburguesa", "Hot Dog", "Torta de asado", "Torta de jam\u00F3n y queso", "Ensalada", "Pizza de pastor"}));
+		platillos.setBounds(26, 117, 140, 22);
+		add(platillos);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Jamaica", "Horchata", "Coca cola", "Limonada", "Agua", "Cerveza"}));
+		comboBox_1.setBounds(26, 188, 140, 22);
+		add(comboBox_1);
 	}
 }
